e55b93e79e1ffd4afa1a19f4960cb6c744f2ad05
Katherine Norton <katherine.zelda.norton@gmail.com>
2021-11-24 17:51:22 -0700

    Created a very simple scheduler that displays
    Currently no functionality to prevent too close of a shift to another person

--- scripts/main.js
+++ scripts/main.js
@@ -41,5 +41,70 @@ function updateRemovePersonIndex() {
 }
 
 function generateSchedule() {
-    
+    // Remove any previous schedules
+    var scheduleContainer = document.querySelector("#schedule-container");
+    scheduleContainer.innerHTML = "";
+    scheduleContainer.appendChild(document.createElement("div"));
+
+    // Get values from the web page
+    var numPerDay = document.querySelector("#num-per-day").value;
+    var startDate = new Date(document.querySelector("#start-date").value);
+    var endDate = new Date(document.querySelector("#end-date").value);
+
+    // Get the difference in dates and convert to weeks
+    var dateDiffMs = endDate - startDate;
+    var weeks = Math.floor(dateDiffMs / 1000 / 60 / 60 / 24 / 7) + 1;
+
+    // Initialize info for iterating through the weeks
+    var iterator = 0;
+    var randomArray = generateRandomArray(peopleNames.length);
+    var currentDay = new Date(startDate);
+    currentDay.setDate(currentDay.getDate() + 1);
+
+    for (var week = 0; week < weeks; week++) {
+        var dayLabel = document.createElement("label");
+        dayLabel.innerHTML = currentDay.toLocaleDateString();
+        scheduleContainer.appendChild(dayLabel);
+        scheduleContainer.appendChild(document.createElement("br"));
+
+        var list = document.createElement("ul");
+
+        for (var person = 0; person < numPerDay; person++) {
+            var personItem = document.createElement("li");
+            personItem.innerHTML = peopleNames[randomArray[iterator]];
+            list.appendChild(personItem);
+            iterator++;
+
+            // Prevent overflow
+            if (iterator >= peopleNames.length) {
+                iterator = 0;
+                randomArray = generateRandomArray(peopleNames.length);
+            }
+        }
+
+        currentDay.setDate(currentDay.getDate() + 7);
+        scheduleContainer.appendChild(list);
+        scheduleContainer.appendChild(document.createElement("br"));
+    }
+}
+
+function generateRandomArray(size) {
+    // Create array of values counting up by index
+    var result = [];
+    for (var i = 0; i < size; i++) {
+        result.push(i);
+    }
+
+    // Durstenfeld's version of Fisher-Yates
+    for (var i = size - 1; i > 0; i--) {
+        // Get value less than the current range
+        var roll = Math.floor(Math.random() * i);
+
+        // Swap the values to make the resut at end of array
+        var temp = result[i];
+        result[i] = result[roll];
+        result[roll] = temp;
+    }
+
+    return result;
 }
